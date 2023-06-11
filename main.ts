13ee29d37b76041fc585a94b12c59cf5e48ed7a7
vacsup <136230087+vacsup@users.noreply.github.com>
2023-06-11 15:45:13 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showNumber(input.temperature())
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showNumber(input.lightLevel())
 })
