3a0749e5804ea0368d7ac45d5fe72126c3df3d32
vacsup <136230087+vacsup@users.noreply.github.com>
2023-06-11 15:43:29 +0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,18 @@
+basic.show_number(10)
+basic.show_leds("""
+    . . . . .
+        . # . # .
+        # . # . #
+        # # # # #
+        . . . . .
+""")
+basic.show_string("wti")
+basic.pause(2000)
+basic.show_leds("""
+    . . . . .
+        . # . # .
+        # . # . #
+        # # # # #
+        . . . . .
+""")
+basic.show_arrow(ArrowNames.WEST)
